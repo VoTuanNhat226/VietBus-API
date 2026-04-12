8ff3aea3a888061e1e283a1e50ac6bd150962d7d
NhatVT2 <NhatVT2@fpt.com>
2026-04-13 02:19:27 +0700

    update TripStatusEnum

--- src/main/java/com/vtn/enumdef/TripStatusEnum.java
+++ src/main/java/com/vtn/enumdef/TripStatusEnum.java
@@ -2,11 +2,9 @@ package com.vtn.enumdef;
 
 public enum TripStatusEnum {
     CREATED,
-    SCHEDULED,
     OPEN_FOR_BOOKING,
     CLOSED_FOR_BOOKING,
     DEPARTED,
-    IN_PROGRESS,
     COMPLETED,
     CANCELLED
 }
