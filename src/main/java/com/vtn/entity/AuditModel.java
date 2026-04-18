fd1d12e0e029836f7bcee54e1dc34abdb6921ea3
HoangTN25 <HoangTN25@fpt.com>
2026-04-18 15:47:31 +0700

    Create an api create passenger

---
+++ src/main/java/com/vtn/entity/AuditModel.java
@@ -0,0 +1,32 @@
+package com.vtn.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditModel {
+
+    @CreatedDate
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+
+    @Column(name = "created_by")
+    String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    String updatedBy;
+}
--- src/main/java/com/vtn/repository/PassengerRepository.java
+++ src/main/java/com/vtn/repository/PassengerRepository.java
@@ -2,8 +2,11 @@ package com.vtn.repository;
 
 import com.vtn.entity.PassengerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PassengerRepository extends JpaRepository<PassengerEntity, UUID> {
+    boolean existsByEmail(String email);
 }
