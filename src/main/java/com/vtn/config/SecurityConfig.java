5c02c49367850906a5e2818d9b6470887ef6290f
NhatVT2 <NhatVT2@fpt.com>
2026-06-21 22:18:02 +0700

    update SecurityConfig

--- src/main/java/com/vtn/config/SecurityConfig.java
+++ src/main/java/com/vtn/config/SecurityConfig.java
@@ -60,9 +60,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(List.of(
-                "http://localhost:5173"
-//                "http://localhost:3000"
-//                "https://your-domain.com"
+                "http://localhost:5173",
+                "https://viet-bus.vercel.app/"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
